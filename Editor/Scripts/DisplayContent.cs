8f884c0070de62d75d5c825f6cbe5c7630e58cfe
Tiogiras <tiogiras@gmail.com>
2024-01-22 15:39:08 +0100

    Initialized Package

---
+++ Editor/Scripts/DisplayContent.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+using UnityEngine.UIElements;
+
+namespace Editor.Scripts
+{
+
+internal static partial class DisplayContent
+{
+    #region Private Methods
+
+    // Called by reflection
+    // ReSharper disable once UnusedMember.Local
+    // ReSharper disable once UnusedParameter.Local
+    private static void NotePad(VisualElement _) { }
+
+    #endregion
+}
+
+}
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MegaPint-NotePad
+Package for the MegaPint package collection that allows for creating validatable MonoBehaviours that can automatically identify and fix occuring issues.
+
+All further information can be found after installing the package via MegaPint.
